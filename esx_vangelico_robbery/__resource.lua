0e810201e0b3d046839785f09890d27220425d93
reinderhe <shev22ppal@live.nl>
2020-05-16 02:56:08 +0200

    first commit

---
+++ esx_vangelico_robbery/__resource.lua
@@ -0,0 +1,11 @@
+client_scripts {
+	"@es_extended/locale.lua",
+	"locales/en.lua",
+    "client/esx_vangelico_robbery_cl.lua",
+	"@utk_fingerprint/client.lua"
+}
+
+server_scripts {
+	"server/esx_vangelico_robbery_sv.lua",
+	'@es_extended/locale.lua'	
+}
